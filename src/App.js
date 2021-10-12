b1b7bdeef77441061c4d0539c7964e5adfb15e93
Kabilan Sakthivelu <kabilansakthi97@gmail.com>
2021-10-12 23:30:25 +0530

    Updated Sign Up page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import SignIn from './Components/SignIn/SignIn';
-import SignUp from './Components/SignUp/SignUp';
+import SignUp from './Components/SignUp';
 import Home from './Components/Home/Home';
 import AddNew from './Components/AddNew/AddNew';
 import Chart from './Components/Chart/Chart';
--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import {FcGoogle} from 'react-icons/fc';
 import {AiOutlineMail, AiOutlineLock} from 'react-icons/ai';
+import {FaUser} from 'react-icons/fa';
 import {Link} from 'react-router-dom';
-import './SignIn.css';
 
-const SignIn = () => {
+const SignUp = () => {
     return (
-        <div className="signInPage">
+         <div className="signInPage">
 
         <div className="signInLeftPanel">
         </div>
@@ -14,9 +13,14 @@ const SignIn = () => {
         <div className="signInModal">
             <h1 className="title">Planner</h1>
             <p className="description">A job well planned is half done</p>
-            <p className="description">Sign in to your account</p>
+            <p className="description">Create a new account</p>
 
             <form className="loginForm">
+
+            <div className="inputFieldDiv">
+            <FaUser className="inputFieldIcon"/>
+            <input className="inputField" required type="text" name="username" id="username" placeholder="Username"/>
+            </div>
 
             <div className="inputFieldDiv">
             <AiOutlineMail className="inputFieldIcon"/>
@@ -28,27 +32,16 @@ const SignIn = () => {
             <input className="inputField" required type="password" name="password" id="password" placeholder="Password"/>
             </div>
 
-            <button className="signInBtn">Sign In</button>
+            <button className="signInBtn">Sign Up</button>
             </form>
 
-            <div className="orDividerDiv">
-            <hr className="orDividerRuler"/>
-            <p className="orDivider">or</p>
-            <hr className="orDividerRuler"/>
-            </div>
-
-            <div className="googleSignInBtn">
-            <FcGoogle className="googleIcon"/>
-            <p className="googleSignInBtnDesc">Continue with Google</p>
-            </div>
-
             <div className="footer">
-            <p>New here?</p>
-            <Link to="/signup" className="signUpLink">Create an account</Link>
+            <p>Already have an account?</p>
+            <Link to="/signin" className="signUpLink">Sign In</Link>
             </div>
         </div>
         </div>
     )
 }
 
-export default SignIn
+export default SignUp
