de4aa1864dd61e98df24d2c4e09bebc04eff4b79
Kabilan Sakthivelu <kabilansakthi97@gmail.com>
2021-10-12 23:15:57 +0530

    Updated Sign in page

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -1,10 +1,53 @@
 import React from 'react';
+import {FcGoogle} from 'react-icons/fc';
+import {AiOutlineMail, AiOutlineLock} from 'react-icons/ai';
+import {Link} from 'react-router-dom';
 import './SignIn.css';
 
 const SignIn = () => {
     return (
-        <div>
-            <h1>Sign In page</h1>
+        <div className="signInPage">
+
+        <div className="signInLeftPanel">
+        </div>
+
+        <div className="signInModal">
+            <h1 className="title">Planner</h1>
+            <p className="description">A job well planned is half done</p>
+
+            {/* Login Form */}
+
+            <form className="loginForm">
+
+            <div className="inputFieldDiv">
+            <AiOutlineMail className="inputFieldIcon"/>
+            <input className="inputField" required type="email" name="email" id="email" placeholder="Email" />
+            </div>
+
+            <div className="inputFieldDiv">
+            <AiOutlineLock className="inputFieldIcon"/>
+            <input className="inputField" required type="password" name="password" id="password" placeholder="Password"/>
+            </div>
+
+            <button className="signInBtn">Sign In</button>
+            </form>
+
+            <div className="orDividerDiv">
+            <hr className="orDividerRuler"/>
+            <p className="orDivider">or</p>
+            <hr className="orDividerRuler"/>
+            </div>
+
+            <div className="googleSignInBtn">
+            <FcGoogle className="googleIcon"/>
+            <p className="googleSignInBtnDesc">Continue with Google</p>
+            </div>
+
+            <div className="footer">
+            <p>New here?</p>
+            <Link to="/signup" className="signUpLink">Create an account</Link>
+            </div>
+        </div>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+         'image': "url('https://startinfinity.s3.us-east-2.amazonaws.com/production/blog/post/5/main/1SvzKctRCi8bwB0QPdOZkBP0pRhsOqZpl0wjs6y0.png')",
+        },
+    },
   },
   variants: {
     extend: {},
