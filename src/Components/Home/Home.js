2425c71bf9a8301362363a6d1fe3e36431ce3d9e
Kabilan Sakthivelu <kabilansakthi97@gmail.com>
2021-10-13 12:47:25 +0530

    Added content for Home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
+import {auth, db} from '../../firebase';
+import {useAuthState} from 'react-firebase-hooks/auth';
+import SignIn from '../SignIn/SignIn';
 import './Home.css';
 
 const Home = () => {
+
+    const [user] = useAuthState(auth);
+
     return (
-        <div>
+        <>
+        {user ? 
+        (<div>
             <Navbar/>
-        </div>
+            <div className="HomePageContent">
+            <p className="description">A job well planned is half done </p>
+            <p className="description">Plan and classify your task for an efficient work</p>
+            <p className="sectionHeader">Your tasks</p>
+            <div className="boardSection">
+            <div className="individualBoard">
+                <h1 className="boardHeader">Next Up</h1>
+                <hr className="headerDivider"/>
+            </div>
+            <div className="individualBoard">
+                <h1 className="boardHeader">In Progress</h1>
+                <hr className="headerDivider"/>
+            </div>
+            <div className="individualBoard">
+                <h1 className="boardHeader">Completed</h1>
+                <hr className="headerDivider"/>
+            </div>
+            </div>
+            </div>
+        </div>)
+        :
+        <SignIn/>
+        }
+        </>
     )
 }
 
