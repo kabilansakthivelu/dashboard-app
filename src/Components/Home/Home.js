1019ab5b9abeb40ee09e389a0540a3d7baca4820
Kabilan Sakthivelu <kabilansakthi97@gmail.com>
2021-10-14 11:17:18 +0530

    Committing changes made in styling

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -19,16 +19,31 @@ const Home = () => {
             <p className="description">Plan and classify your tasks for an efficient work</p>
             <p className="sectionHeader">Your tasks</p>
             <div className="boardSection">
+
+            {/* Next Up board */}
+
             <div className="individualBoard">
                 <h1 className="boardHeader">Next Up</h1>
                 <hr className="headerDivider"/>
                 {nextUpTasks.map((task)=>{ 
 
                     const time1 = task.time.split(" ");
-                    const time = time1[1] + " " + time1[0]
+                    const time = time1[1] + " " + time1[0];
+
+                    let taskClassName;
+
+                    if(task.priority === "low"){
+                        taskClassName = "taskLowPriority";
+                    }
+                    else if(task.priority === "medium"){
+                        taskClassName = "taskMediumPriority";
+                    }
+                    else{
+                        taskClassName = "taskHighPriority";
+                    }
 
                     return (<div key={task.id} className="singleTask">
-                        <h1 className="taskPriority">{task.priority}</h1>
+                        <h1 className={taskClassName}>{task.priority}</h1>
                         <h1 className="taskTitle">{task.taskName}</h1>
                         <h1 className="taskComments">{task.comments}</h1>
                         <div className="taskTime">
@@ -38,10 +53,16 @@ const Home = () => {
                     </div>)
                 })}
             </div>
+
+            {/* In Progress board */}
+
             <div className="individualBoard">
                 <h1 className="boardHeader">In Progress</h1>
                 <hr className="headerDivider"/>
             </div>
+
+            {/* Completed board */}
+
             <div className="individualBoard">
                 <h1 className="boardHeader">Completed</h1>
                 <hr className="headerDivider"/>
